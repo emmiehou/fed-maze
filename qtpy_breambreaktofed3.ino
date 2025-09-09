895b6f0e4e5ec42e263980761f495a62e94912e8
emmiehou <97652450+emmiehou@users.noreply.github.com>
2025-09-08 20:33:00 -0700

    QTpy external circuit 9/8/25

---
+++ qtpy_breambreaktofed3.ino
@@ -0,0 +1,50 @@
+/*
+ Arudino QTPY external circuit in maze
+ Last updated 8/25/25
+ Emmie Hou
+
+ This will send BNC signals out of A0 when the beam is broken
+*/
+#define SENSORPIN A2
+// variables will change:
+int sensorState = 0, lastState=0; // variable for reading the pushbutton
+static
+
+//pin for bnc output
+#define BEH   A0
+
+//how to send BNC signal
+void sendBNC(int ms) {
+  digitalWrite(BEH, HIGH);
+  delay(ms);
+  digitalWrite(BEH, LOW);
+}
+
+void setup() {
+  // initialize the LED pin as an output:
+  //pinMode(LEDPIN, OUTPUT);
+  // initialize the sensor pin as an input:
+  pinMode(SENSORPIN, INPUT_PULLUP);
+  pinMode(BEH, OUTPUT);
+  digitalWrite(BEH, LOW);  // Start LOW
+  Serial.begin(9600);
+}
+
+void loop(){
+  // read the state of the pushbutton value:
+  sensorState = digitalRead(SENSORPIN);
+  Serial.print(digitalRead(SENSORPIN));
+  // check if the sensor beam is broken
+  // if it is, the sensorState is LOW, send BNC signal and turn LED on:
+  if (sensorState == LOW) {
+    // send BNC signal:
+    sendBNC(1000);
+    // turn LED on:
+    //digitalWrite(LEDPIN, HIGH);
+  }
+  else {
+    // turn LED off:
+    //digitalWrite(LEDPIN, LOW);
+  lastState = sensorState;
+  }
+}
