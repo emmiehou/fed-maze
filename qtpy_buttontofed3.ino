5cf771ca3358388d48106b61c3a16b793feccab9
emmiehou <97652450+emmiehou@users.noreply.github.com>
2025-09-10 10:23:24 -0700

    Add files via upload

--- qtpy_buttontofed3.ino
+++ qtpy_buttontofed3.ino
@@ -9,7 +9,8 @@
 */
 #define BUTTONPIN A2
 // variables will change:
-int buttonState = 0, lastState=0; // variable for reading the pushbutton
+int buttonState = 0;
+int lastState = 0; // variable for reading the pushbutton
 
 //pin for bnc output
 #define BEH   A0
@@ -24,22 +25,19 @@ void sendBNC(int ms) {
 void setup() {
   // initialize the sensor pin as an input:
   pinMode(BEH, OUTPUT);
-  pinMode(BEH, LOW)
+  digitalWrite(BEH, LOW);
   // initialize button pin as an input
-  pinMode(BUTTONPIN, INPUT);
+  pinMode(BUTTONPIN, INPUT_PULLUP);
   Serial.begin(9600);
 }
 
-void loop(){
-  // read the state of the pushbutton value:
+void loop() {
   buttonState = digitalRead(BUTTONPIN);
 
-  // if the button is pressed, send BNC
-  if (buttonState == HIGH) {
-    // send BNC signal:
+  // Trigger only when pressed (edge detection)
+  if (buttonState == LOW && lastState == HIGH) {
     sendBNC(1000);
   }
-  else {
+
   lastState = buttonState;
-  }
 }
